882b7a8b7dd25677d55cbf6a04ee2c426249bac6
Tom Collier <collier@apartmentlist.com>
2015-10-20 16:59:34 -0700

    Update README to reflect CodeClimate instructions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'enumerated_constants'
-require 'codeclimate-test-reporter'
-
-CodeClimate::TestReporter.start
